1c4ff1e613e0c8ae95dd817d4d418ccf1e99ba31
Facu Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-10-13 19:20:20 -0300

    Create fav.py
    favorite tweet

---
+++ fav.py
@@ -0,0 +1,21 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler('API key','API key secret')
+
+auth.set_access_token('Access Token','Access token secret')
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+search = "#HastagExample"
+nrTweets = 1000
+for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+    try:
+        print('Tweet Liked and Retweet')
+        tweet.favorite()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
